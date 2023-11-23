e8e88afc953a104ddb2d688702757c1275eab9ce
jpalianak <100520765+jpalianak@users.noreply.github.com>
2023-11-23 18:21:01 -0300

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from dynamodb_connection import DynamoDBConnection
 
 # Create a connection:
-conn = st.experimental_connection("dynamodb_results", type=DynamoDBConnection, api_type="pandas")
+conn = st.experimental_connection("dynamodb_results", type=DynamoDBConnection, api_type="pandas", table_name="dynamodb_results")
 
 # Get all items in the table:
 st.write(conn.items())
