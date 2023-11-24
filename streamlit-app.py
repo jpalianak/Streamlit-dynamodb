01cb5be169e7f2a921772ebdcd455465a3074025
jpalianak <100520765+jpalianak@users.noreply.github.com>
2023-11-24 17:43:15 -0300

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -60,6 +60,7 @@ df_last = compute_movement()
 
 # Reemplazamos el contenido del placeholder con la 
 placeholder.line_chart(data=df_last, x='Date', y='Ratio', color=["#FF0000"], width=800, height=400, use_container_width=False)
+st.markdown("### Detailed Data View")
 st.dataframe(df_last)
 
     # Actualizamos cada 1 minutos
