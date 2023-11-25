41d8f6263527ee4a2c6d24011193d8329bce3b50
jpalianak <100520765+jpalianak@users.noreply.github.com>
2023-11-24 21:16:13 -0300

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -8,7 +8,7 @@ import plotly.express as px # interactive charts
 # Creamos un placeholder inicial vacío
 placeholder = st.empty()
 
-st.set_option('wideMode' , True)
+st.set_page_config(layout="wide")
 
 def get_data():
   # Crear el cliente de DynamoDB usando boto3
