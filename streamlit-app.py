decb4eace12aa5d1c7f0d08aa781aa01c51dacb4
jpalianak <100520765+jpalianak@users.noreply.github.com>
2023-11-26 16:26:02 -0300

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -65,7 +65,7 @@ with placeholder.container():
     st.sidebar.markdown('## Seleccione los parametros de visualización')
 
     # create three columns
-    kpi1_1, kpi1, kpi2_2, kpi2, kpi3_3, kpi3 = st.columns([.033, .3, .033, .3, .033, .3])
+    row0_spacer1, kpi1, row0_spacer2, kpi2, row0_spacer3, kpi3 = st.columns((.1, 3, .1, 3, .1, 3))
 
     # fill in those three columns with respective metrics or KPIs 
     kpi1.metric(label="Pruductividad diaria ⏳", value=100, delta=80)
