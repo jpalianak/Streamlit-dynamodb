cd8a81ffc460c8bfe130f93072da5141189c4d49
jpalianak <100520765+jpalianak@users.noreply.github.com>
2023-12-01 19:59:30 -0300

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -42,9 +42,7 @@ def compute_movement(maquina,d_ini,d_fin):
   print(d_ini.format)
         
   # Filtro del dataframe segun los parametros seleccionados
-  df_filter = df_orig[(df_orig['Date_num'] >= pd.to_datetime(d_ini)) & (df_orig['Date_num'] <= pd.to_datetime(d_fin))]
-
-  df_new = pd.DataFrame()
+  df_filter = df_orig[(df_orig['Date_num'] >= pd.to_datetime(d_ini).astype('int64')) & (df_orig['Date_num'] <= pd.to_datetime(d_fin).astype('int64'))]
 
   df_new['Date'] = df_filter['Date'].iloc[1:]
   df_new['Date_diff'] = df_filter['Date_num'] - df_filter['Date_num'].shift()
