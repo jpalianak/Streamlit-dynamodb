9f69fe248f2bcf46a588857ff764826242233419
jpalianak <100520765+jpalianak@users.noreply.github.com>
2023-12-01 20:46:00 -0300

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -75,14 +75,14 @@ with placeholder.container():
               min_value=pd.to_datetime(df_orig['Date'].min()),
               max_value=pd.to_datetime(df_orig['Date'].max()),
               value=pd.to_datetime(df_orig['Date'].min())
-      st.write('Desde: ', d_ini)
+      #st.write('Desde: ', d_ini)
       d_ini_num = pd.to_datetime(d_ini).astype('int64') // 10**9
       
       d_fin = st.date_input("Periodo a evaluar. Desde:", 
               min_value=pd.to_datetime(df_orig['Date'].min()),
               max_value=pd.to_datetime(df_orig['Date'].max()),
               value=pd.to_datetime(df_orig['Date'].max())
-      st.write('Hasta: ', d_fin)
+      #st.write('Hasta: ', d_fin)
       d_fin_num = pd.to_datetime(d_fin).astype('int64') // 10**9
 
     # Obtenemos los nuevos datos
