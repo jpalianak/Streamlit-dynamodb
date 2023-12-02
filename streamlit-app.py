dbaeabc9ee2dce2bbf30a71801c48e148d974e61
jpalianak <100520765+jpalianak@users.noreply.github.com>
2023-12-02 19:44:23 -0300

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -82,7 +82,7 @@ with placeholder.container():
     with st.sidebar:
       # Titulo del Sidebar  
       #st.header('AIRBIZ', divider='rainbow')
-      st.header(r"$$\huge AIRBIZ$$")
+      st.header(r"$\huge AIRBIZ$")
       st.header(':blue[_Productivity dasboard_] :factory:')
       st.write('')
       st.write('')
