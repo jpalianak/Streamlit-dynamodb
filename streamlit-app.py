6fad6b5e6c3f597e09bd5dc332c928a9d7e8331b
jpalianak <100520765+jpalianak@users.noreply.github.com>
2023-12-02 08:15:05 -0300

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -26,7 +26,6 @@ def get_data():
   # Convertir los datos a un DataFrame de Pandas
   df = pd.DataFrame(items)
   df['Date_num'] = pd.to_datetime(df['Date']).astype('int64') // 10**9
-  #df['Date'] = pd.to_datetime(df['Date']).dt.date
   df = df.sort_values(by='Date_num')
   return df
   
@@ -39,25 +38,40 @@ def compute_movement(df_orig,maquina,d_ini,d_fin):
   # Filtro del dataframe segun los parametros seleccionados
   df_filter = df_orig[(pd.to_datetime(df_orig['Date']).dt.date >= d_ini) & (pd.to_datetime(df_orig['Date']).dt.date <= d_fin)]
 
+  # Crear un nuevo dataframe
   df_new = pd.DataFrame()
 
+  # Calculo de la diferencia de tiempo entre registro
   df_new['Date'] = df_filter['Date'].iloc[1:]
   df_new['Date_diff'] = df_filter['Date_num'] - df_filter['Date_num'].shift()
 
-  threshold = 0.001
+  # Calculo de la diferencia en X e Y de los centros de cada registro
   xcenter_diff = abs(df_filter['Xcenter'] -
                      df_filter['Xcenter'].shift()).iloc[1:]
   ycenter_diff = abs(df_filter['Ycenter'] -
                      df_filter['Ycenter'].shift()).iloc[1:]
 
+  # umbral de diferencia de posicion del centro del bounding box para determinar si esta en movimieno o no
+  threshold = 0.001
+  
+  # Deteminar movimiento o no. Si la diferencia en X o en Y es mayor al threshold se esta moviendo, si es menor esta detenida.
   df_new['Movement'] = ['SI' if x > threshold or y >
                         threshold else 'NO' for x, y in zip(xcenter_diff, ycenter_diff)]
 
+  # Mascara de los registros que tienen SI en movimiento.
   si_mask = (df_new['Movement'] == 'SI').astype(int).tolist()
+
+  # Nueva columna con cero donde el movimiento es NO y Date_diff donde el movimiento es SI.
   df_new['Date_diff_aux'] = [delta * mask if mask else 0 for delta,
                              mask in zip(df_new['Date_diff'], si_mask)]
+
+  # Acumulado de tiempo de los registros con movimiento SI
   df_new['Cumulative_sum_si'] = df_new['Date_diff_aux'].cumsum()
+
+  # Acumulado de tiempo total
   df_new['Cumulative_total'] = df_new['Date_diff'].cumsum()
+
+  # Porcentaje de productividad
   df_new['Ratio'] = df_new['Cumulative_sum_si'] / df_new['Cumulative_total']
   return df_new
 
@@ -66,28 +80,30 @@ with placeholder.container():
     df_orig = get_data()
   
     with st.sidebar:
-        st.sidebar.markdown('## Seleccione los parametros de visualización')
-        maquina = st.selectbox(
-          'Seleccione la maquina:',
-          ('Maquina 1', 'Maquina 2', 'Maquina 3'))
-        st.write('Maquina seleccionada:', maquina)
-        st.write('')
-        st.write('')
-        d_ini = st.date_input("Periodo a evaluar. Desde:",value='today')
-        st.write('Desde: ', d_ini)
-        #d_ini_num = pd.to_datetime(d_ini).astype('int64') // 10**9
-      
-        d_fin = st.date_input("Periodo a evaluar. Hasta:",value='today')
-        st.write('Hasta: ', d_fin)
-        #d_fin_num = pd.to_datetime(d_fin).astype('int64') // 10**9
+      # Titulo del Sidebar  
+      st.sidebar.markdown('## Seleccione los parametros de visualización')
+
+      # Selecbox para la seleccion de maquina
+      maquina = st.selectbox('Seleccione la maquina:',('Maquina 1', 'Maquina 2', 'Maquina 3'))
+      st.write('Maquina seleccionada:', maquina)
+      st.write('')
+      st.write('')
+
+      # Box para seleccionar la fecha inicial
+      d_ini = st.date_input("Periodo a evaluar. Desde:",value='today')
+      st.write('Desde: ', d_ini)
+
+      # Box para seleccionar la fecha final
+      d_fin = st.date_input("Periodo a evaluar. Hasta:",value='today')
+      st.write('Hasta: ', d_fin)
 
     # Obtenemos los nuevos datos
     df_last = compute_movement(df_orig,maquina,d_ini,d_fin)
   
-    # create three columns
+    # Creacion de 3 columnas
     row0_spacer1, kpi1, row0_spacer2, kpi2, row0_spacer3, kpi3 = st.columns((.5, 3, .1, 3, .1, 3))
 
-    # fill in those three columns with respective metrics or KPIs 
+    # Creacion de los 3 KPIs, uno por cada columna creada
     with kpi1:
         kpi1.metric(label="### Productividad Diaria", value="100%", delta=80)
     with kpi2:
@@ -97,18 +113,18 @@ with placeholder.container():
     st.write('')
     st.write('')
 
-    # create two columns for charts 
     row1_spacer1,fig_col1,row1_spacer2 = st.columns((.3, 10, .1,))
   
     with fig_col1:
-        df_last['Ratio'] = df_last['Ratio'] *100
-        st.markdown("### Evolucion de la Productividad")
-        fig = px.line(data_frame=df_last, x='Date', y='Ratio',markers=True)
-        fig.update_layout(width=1200)
-        fig.update_layout(height=400)
-        st.write(fig)
+      df_last['Ratio'] = df_last['Ratio'] *100
+      st.write('')
+      st.write('')
+      st.markdown("### Evolucion de la Productividad")
+      fig = px.line(data_frame=df_last, x='Date', y='Ratio',markers=True)
+      fig.update_layout(width=1200)
+      fig.update_layout(height=400)
+      st.write(fig)
 
-    # create two columns for charts 
     row2_spacer1,df_col1,row2_spacer2 = st.columns((.3, 10, .1,))
   
     with df_col1:
