c392023985ececc9308dcf6bd20a4e0ed0a20394
jpalianak <100520765+jpalianak@users.noreply.github.com>
2023-12-03 12:17:36 -0300

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -101,7 +101,7 @@ with placeholder.container():
     df_last = compute_movement(df_orig,maquina,d_ini,d_fin)
 
     # Titulo del Sidebar  
-    st.header(r"$\small \color{white} \textbf{PRODUCTIVITY DASHBOARD}$", divider='gray')
+    st.header(r"$\small \color{white} \textbf{Productivity Dashboard}$", divider='gray')
     #st.header(r"$\tiny \color{#228B22} \textit{by AIRBIZ}$")
     st.write('')
   
