112eb293b0c67c100b7ae368beccf2fd5a68454e
jpalianak <100520765+jpalianak@users.noreply.github.com>
2023-12-08 17:02:31 -0300

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -94,7 +94,7 @@ def compute_movement(df_orig,maquina,d_ini,d_fin):
 def line_graphic_maq(df_orig,maq,d_ini,d_fin): 
   df_last = compute_movement(df_orig,maq,d_ini,d_fin)
   fig = px.line(data_frame=df_last, x='Date', y='Ratio',markers=True)
-  fig.update_layout(xaxis_title="Date", yaxis_title="Productivity [%]",width=600,height=350)
+  fig.update_layout(xaxis_title="Date", yaxis_title="Productivity [%]",width=700,height=350)
   fig.update_yaxes(range=[0, 100]) 
   return fig
 
@@ -181,7 +181,7 @@ with Maq1:
     d_ini = pd.to_datetime(hoy).date()
     d_fin = pd.to_datetime(hoy).date()
     fig = line_graphic_maq(df_orig,Maquina,d_ini,d_fin)
-    fig.update_layout(xaxis_title="Date", yaxis_title="Daily Productivity [%]",width=1200,height=350)
+    fig.update_layout(xaxis_title="Date", yaxis_title="Daily Productivity [%]",width=1400,height=350)
     st.write(fig)
     
     row1_col1, row1_col2 = st.columns((3, 3))
