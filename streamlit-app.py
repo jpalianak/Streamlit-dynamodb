23241a931236b8ea3a55d1deac0b9ef67a2ea3d0
jpalianak <100520765+jpalianak@users.noreply.github.com>
2023-12-08 12:58:30 -0300

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -85,22 +85,8 @@ st.write('')
 # Footer
 footer="""<style>
 a:link , a:visited{color: blue;background-color: transparent;text-decoration: underline;}
-
-a:hover,  a:active {
-color: red;
-background-color: transparent;
-text-decoration: underline;
-}
-
-.footer {
-position: fixed;
-left: 0;
-bottom: 0;
-width: 100%;
-background-color: white;
-color: black;
-text-align: center;
-}
+a:hover,  a:active {color: red;background-color: transparent;text-decoration: underline;}
+.footer {position: fixed;left: 0;bottom: 0;width: 100%;background-color: white;color: black;text-align: center;}
 </style>
 <div class="footer">
 <p>Developed by AIRBIZ <a style='display: block; text-align: center;' href="https://www.airbiz.com/" target="_blank">www.airbiz.com</a></p>
