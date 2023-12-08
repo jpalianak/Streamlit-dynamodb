1feb109c41a75b615cea2a7dd8c08f4f86c1c978
jpalianak <100520765+jpalianak@users.noreply.github.com>
2023-12-08 13:21:03 -0300

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -127,7 +127,7 @@ fin_mes = siguiente_mes - datetime.timedelta(days=1)
 Main, Maq1, Maq2, Maq3, Maq4, Maq5 = st.tabs(["Main", "Maquina 1", "Maquina 2", "Maquina 3", "Maquina 4", "Maquina 5"])
 
 with Maq1:
-  maq = "maq1"
+  Maquina = "maq1"
   
   st.write('')
   row0_spacer1, row0_col1, row0_spacer2, row0_col2, row0_spacer3, row0_col3 = st.columns((1.8, 3, 1.5, 3, 1.5, 3))
@@ -139,25 +139,17 @@ with Maq1:
   with row1_col1:
     d_ini = pd.to_datetime(hoy).date()
     d_fin = pd.to_datetime(hoy).date()
-    fig = line_graphic(df_orig,maq,d_ini,d_fin)
+    fig = line_graphic(df_orig,Maquina,d_ini,d_fin)
     st.write(fig)
   with row1_col2:
     d_ini = pd.to_datetime(inicio_semana_laboral).date()
     d_fin = pd.to_datetime(fin_semana_laboral).date()
-    df_last = compute_movement(df_orig,maq,d_ini,d_fin)
-    fig = px.line(data_frame=df_last, x='Date', y='Ratio',markers=True)
-    fig.update_layout(width=550)
-    fig.update_layout(height=400)
-    fig.update_yaxes(range=[0, 100]) 
+    fig = line_graphic(df_orig,Maquina,d_ini,d_fin) 
     st.write(fig)
   with row1_col3:
     d_ini = pd.to_datetime(inicio_mes).date()
     d_fin = pd.to_datetime(fin_mes).date()
-    df_last = compute_movement(df_orig,maq,d_ini,d_fin)
-    fig = px.line(data_frame=df_last, x='Date', y='Ratio',markers=True)
-    fig.update_layout(width=550)
-    fig.update_layout(height=400)
-    fig.update_yaxes(range=[0, 100]) 
+    fig = line_graphic(df_orig,Maquina,d_ini,d_fin)
     st.write(fig)
 time.sleep(1)   
     
