fcf9135f61ec83b1fa1820a66c735b01b0232e64
jpalianak <100520765+jpalianak@users.noreply.github.com>
2023-12-08 10:16:10 -0300

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -75,70 +75,10 @@ def compute_movement(df_orig,maquina,d_ini,d_fin):
   df_new['Ratio'] = df_new['Cumulative_sum_si'] / df_new['Cumulative_total']
   return df_new
 
-with placeholder.container():
-    # Obtenemos los datos
-    df_orig = get_data()
+# Obtenemos los datos
+df_orig = get_data()
   
-    with st.sidebar:   
-      st.divider() 
-      
-      # Selecbox para la seleccion de maquina
-      st.markdown(r"$\normalsize \color{white} \textbf{Seleccione la máquina}$")
-      maquina = st.selectbox('',('Maquina 1', 'Maquina 2', 'Maquina 3'))
-      #st.write('Maquina seleccionada:', maquina)
-      
-      st.divider() 
-      
-      # Box para seleccionar la fecha inicial
-      st.markdown(r"$\normalsize \color{white} \textbf{Periodo a evaluar}$")
-      d_ini = st.date_input("Desde:",value='today')
-      #st.write('Desde: ', d_ini)
-      # Box para seleccionar la fecha final
-      d_fin = st.date_input("Hasta:",value='today')
-      #st.write('Hasta: ', d_fin)
-
-      st.divider()
-
-    # Obtenemos los nuevos datos
-    df_last = compute_movement(df_orig,maquina,d_ini,d_fin)
-
-    # Titulo del Sidebar  
-    st.header(r"$\small \color{white} \textbf{Productivity Dashboard} \tiny \color{#228B22} \textit{ by AIRBIZ}$", divider='gray')
-    #st.header(r"$\tiny \color{#228B22} \textit{by AIRBIZ}$")
-    st.write('')
-  
-    # Creacion de 3 columnas
-    row0_spacer1, kpi1, row0_spacer2, kpi2, row0_spacer3, kpi3 = st.columns((.5, 3, .1, 3, .1, 3))
-
-    # Creacion de los 3 KPIs, uno por cada columna creada
-    with kpi1:
-        kpi1.metric(label="### Productividad Diaria", value="100%", delta=80)
-    with kpi2:
-        kpi2.metric(label="Productividad Semanal", value="100%", delta=-50)
-    with kpi3:
-        kpi3.metric(label="Productividad Mensual", value="100%", delta=10)
-    st.write('')
-    st.write('')
-
-    row1_spacer1,fig_col1,row1_spacer2 = st.columns((.3, 10, .1,))
-  
-    with fig_col1:
-      df_last['Ratio'] = df_last['Ratio'] *100
-      st.write('')
-      st.write('')
-      #st.markdown("### Evolucion de la Productividad")
-      fig = px.line(data_frame=df_last, x='Date', y='Ratio',markers=True,title='Evolucion de la Productividad')
-      fig.update_layout(width=1200)
-      fig.update_layout(height=400)
-      fig.update_yaxes(range=[0, 100]) 
-      st.write(fig)
-
-    row2_spacer1,df_col1,row2_spacer2 = st.columns((.3, 10, .1,))
-  
-    with df_col1:
-        st.markdown("### Tabla de registros")
-        st.dataframe(df_last,width=1200, height=500)
-        #st.dataframe(df_last.tail(5),width=1200, height=200)
+Maq1, Maq1, Maq1 = st.tabs(["Maquina 1", "Maquina 2", "Maquina 3"])
    
-    time.sleep(1)   
+time.sleep(1)   
     
